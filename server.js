a7c52f4b2829bd1717a6dc507a66c00e70e87bcb
CodeSandbox <support@codesandbox.io>
2025-06-25 07:08:38 +0000

    Sandbox snapshot commit

--- server.js
+++ server.js
@@ -5,6 +5,5 @@ class Emitter extends EventEmitter {}
 const myEmitter = new Emitter();
 // add listener for the log event
 myEmitter.on("log", (msg) => logEvents(msg));
-setTimeout(() => {
-  myEmitter.emit("log", "Log event emitted");
-}, 2000);
+
+myEmitter.emit("log", "Log event emitted");
