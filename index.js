a992c1bfe9331e2e1d4395dce3be94b918f49094
CodeSandbox <support@codesandbox.io>
2025-06-25 04:52:04 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ const { error } = require("console");
 const path = require("path");
 const filePromises = require("fs").promises;
 
-// console.log(path.dirname(__filename));
-// console.log(path.extname(__filename));
+const { format } = require("date-fns");
+console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
+console.log("hello");
+// // console.log(path.dirname(__filename));
+// // console.log(path.extname(__filename));
 // console.log(path.basename(__filename));
 
-const fileOps = async () => {
-  try {
-    const data = await filePromises.readFile(
-      path.join(__dirname, "files", "starter.txt"),
-      "utf8"
-    );
-    console.log(data);
-    await filePromises.writeFile(
-      path.join(__dirname, "files", "reply.txt"),
-      data
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-fileOps();
+// const fileOps = async () => {
+//   try {
+//     const data = await filePromises.readFile(
+//       path.join(__dirname, "files", "starter.txt"),
+//       "utf8"
+//     );
+//     console.log(data);
+//     await filePromises.writeFile(
+//       path.join(__dirname, "files", "reply.txt"),
+//       data
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// fileOps();
