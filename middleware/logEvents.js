5b70cc619bfd80f76df056d53f136b877221dd58
CodeSandbox <support@codesandbox.io>
2025-07-17 14:28:53 +0000

    Sandbox snapshot commit

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -21,26 +21,3 @@ exports.logEvents = async (message) => {
     console.log(error);
   }
 };
-//
-// const { error } = require("console");
-
-// // console.log(path.dirname(__filename));
-// // console.log(path.extname(__filename));
-// console.log(path.basename(__filename));
-
-// const fileOps = async () => {
-//   try {
-//     const data = await filePromises.readFile(
-//       path.join(__dirname, "files", "starter.txt"),
-//       "utf8"
-//     );
-//     console.log(data);
-//     await filePromises.writeFile(
-//       path.join(__dirname, "files", "reply.txt"),
-//       data
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// fileOps();
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
-const { logEvents } = require("./logEvents");
-const EventEmitter = require("events");
-class Emitter extends EventEmitter {}
-// Initials object
-const myEmitter = new Emitter();
-// add listener for the log event
-myEmitter.on("log", (msg) => logEvents(msg));
+const express = require("express");
+const app = express();
+const path = require("path");
 
-myEmitter.emit("log", "Log event emitted");
+const PORT = process.env.PORT || 3500;
+//
+//
+// serve static file
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("subdir", express.static(path.join(__dirname, "/public")));
+//
+
+app.use("/", require("./routes/root"));
+
+app.all("/*splat", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("text").send("404 Not Found");
+  }
+});
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server running on Port ${PORT}`);
+});
