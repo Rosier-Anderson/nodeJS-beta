3e600faf356540dfbd7a913c5117ff2ec5bf4809
CodeSandbox <support@codesandbox.io>
2025-07-19 05:16:26 +0000

    Sandbox snapshot commit

--- server.js
+++ server.js
@@ -3,20 +3,40 @@ const app = express();
 const path = require("path");
 const errorHandler = require("./middleware/errorHandler");
 const { logger } = require("./middleware/logEvents");
-const cors = cors();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3500;
-app.use(cors());
+
 //
 //
 // serve static file
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/subdir", express.static(path.join(__dirname, "/public")));
-//
 
+const whiteList = [
+  "http://localhost:3500",
+  "https://ghrccp-3500.csb.app",
+  "https://codesandbox.io/",
+  undefined,
+];
+const corsOption = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      console.log(` CORS: ${origin}`);
+      callback(null, true);
+    } else {
+      console.warn(`Blocked by CORS: ${origin}`);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionSucessStatus: 200,
+};
+
+app.use(cors(corsOption));
 app.use(logger);
 app.use("/", require("./routes/root"));
 app.use("/subdir", require("./routes/subdir"));
+
 app.all("/*splat", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
