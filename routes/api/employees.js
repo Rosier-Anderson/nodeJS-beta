e580baa6e9bafa09ecd05db8059e7b390fb46dbe
CodeSandbox <support@codesandbox.io>
2025-07-20 16:28:32 +0000

    Sandbox snapshot commit

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const data = {};
+data.employees = require("../../model/employees.json");
+
+router.get("/", (req, res) => {
+  res.json(data.employees);
+});
