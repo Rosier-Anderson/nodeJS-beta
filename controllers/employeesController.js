a3a13378035807f214495d5900b82f1ca2e36b8e
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-07-22 20:18:31 -0400

    Update employee controller and logs
    - Refined logic in employeesController.js
    - Updated error and request logs for recent changes

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,15 +1,29 @@
-const data = { employees: require("../model/employees.json") };
-
+const data = {
+  employees: require("../model/employees.json"),
+  setEmployeees: function (newEmployee) {
+    this.employees = [...this.employees, newEmployee];
+    return this.employees;
+  },
+};
+let id = data.employees.at(-1)?.id;
 const getAllEmployees = (req, res) => {
   res.json(data.employees);
-  console.log([req.body], "get");
+  console.log("get");
 };
 const createNewEmployee = (req, res) => {
-  res.json({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-  });
-  console.log([req.body],"post");
+  if (!req.body.firstName || !req.body.lastName ) {
+    console.log("Must provide a firstName and a lastName");
+  } else if (!data.employees.includes(id)) {
+   
+    data.setEmployeees({
+      id: id++,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+  }
+
+  res.json(data.employees);
+  console.log("post");
 };
 const updateEmployee = (req, res) => {
   res.json({
