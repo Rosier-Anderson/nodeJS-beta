50d4af03fe5746ad0d7d58230cd6628bcbf322c2
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-07-24 19:46:43 -0400

    update employee controller and logs

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,8 +1,7 @@
 const data = {
   employees: require("../model/employees.json"),
-  setEmployeees: function (newEmployee) {
-    this.employees = [...this.employees, newEmployee];
-    return this.employees;
+  setEmployeees: function (data) {
+    this.employees = data;
   },
 };
 let id = data.employees.at(-1)?.id || 1;
@@ -11,26 +10,39 @@ const getAllEmployees = (req, res) => {
   console.log("get");
 };
 const createNewEmployee = (req, res) => {
-  if (!req.body.firstname || !req.body.lastname) {
+  if (!req.body.firstName || !req.body.lastName) {
     console.log("Must provide a firstname and a lastname");
   }
-
+  let newEmployee = {
+    id: req.body.id,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+  let s = data.employees;
   data.setEmployeees({
-    id: (id += 1),
-    firstName: req.body.firstname,
-    lastName: req.body.lastname,
+   ...s,
+   ...newEmployee,
   });
-
+  console.log(data.employees, "Post");
   res.json(data.employees);
 };
 const updateEmployee = (req, res) => {
   // update the employee
-  let employeeId = req.body;
-  let selectedEmployee = data.employees.at(employeeId).id;
-  if (!employeeId) {
-    console.log("Must provide the employee ID");
-  }
-  console.log(employeeId, "selected");
+  let employeeId = req.body.id;
+  let selectedEmployee = data.employees.find((id) => id.id === employeeId);
+  let some = {
+    id: req.body.id,
+    firstname: req.body.firstName,
+    lastname: req.body.lastName,
+  };
+  if (!employeeId) console.log("Must provide the employee ID to update");
+  if (!selectedEmployee) console.log("This employee ID does not exists");
+  let updatedEmployee = {
+    ...selectedEmployee,
+    ...some,
+  };
+  let newData = { ...data.employees, [employeeId]: { updatedEmployee } };
+  console.log(newData, "selected");
   res.json();
   console.log("put");
 };
